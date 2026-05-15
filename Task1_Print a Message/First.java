07167ecf136420cad41045283942f3fdec56cf8d
J.Thibakar <thiba2006077@gmail.com>
2026-05-15 11:29:45 +0530

    Delete First.java

--- Task1_Print a Message/First.java
+++
@@ -1,7 +0,0 @@
-class First{
-	public static void main(String args[]){
-		System.out.println("welcome to yarl it");
-		System.out.println("welcome to yarl it");
-		System.out.println("welcome to yarl it");
-	}
-}
